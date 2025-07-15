91d81e2b77501b08309a0c4fd0c29232f3bdf8f3
hanipubaidur <hanifurohman05@gmail.com>
2025-07-15 16:13:19 +0700

    fix: Memperbaiki pengambilan data transaksi agar lebih akurat dan efisien.

--- api/get_transactions.php
+++ api/get_transactions.php
@@ -18,11 +18,14 @@ try {
                     WHEN t.type = 'expense' AND e.category_name = 'Salary' AND emp.name IS NOT NULL
                         THEN CONCAT(e.category_name, ' - ', emp.name)
                     ELSE e.category_name
-                END as category
+                END as category,
+                t.shift_id,
+                s.name as shift_name
               FROM transactions t
               LEFT JOIN income_sources i ON t.income_source_id = i.id
               LEFT JOIN expense_categories e ON t.expense_category_id = e.id
               LEFT JOIN employees emp ON t.employee_id = emp.id
+              LEFT JOIN shifts s ON t.shift_id = s.id
               WHERE t.status = 'completed'
               ORDER BY t.date DESC, t.id DESC";
 
